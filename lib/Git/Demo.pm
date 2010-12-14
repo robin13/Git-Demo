a6683e286b847e46cf2e50f6d99866f158124c23
Robin Clarke <robin@robinclarke.net>
2010-12-14 07:58:40 +0100

    Samples and other improvements

--- lib/Git/Demo.pm
+++ lib/Git/Demo.pm
@@ -1,11 +1,17 @@
 package Git::Demo;
-
-use warnings;
 use strict;
+use warnings;
+use Git::Repository;
+use Git::Demo::Story;
+
+use Log::Log4perl;
+use File::Util;
+use IO::File;
+use File::Temp;
 
 =head1 NAME
 
-Git::Demo - The great new Git::Demo!
+Git::Demo - A way for scripting git demonstrations
 
 =head1 VERSION
 
@@ -18,35 +24,91 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+Allows playback of a Git story (a sequence of file modifications and git actions
+by various characters) to demonstrate the capabilities of Git
 
 Perhaps a little code snippet.
 
     use Git::Demo;
-
-    my $foo = Git::Demo->new();
     ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+    my $demo = Git::Demo->new( $conf );
+    $demo->play();
 
 =head1 SUBROUTINES/METHODS
 
-=head2 function1
+=head2 new
 
 =cut
 
-sub function1 {
+sub new{
+    my $class = shift;
+    my $args = shift;
+
+    foreach( qw/story_file/ ){
+        if( ! $args->{$_} ){
+            die( "Cannot start without $_ being defined\n" );
+        }
+    }
+
+    my $self = {};
+
+    # and the optionals
+    foreach( qw/verbose/ ){
+        $self->{$_} = $args->{$_};
+    }
+
+    $self->{conf} = $args;
+    my $logger = Log::Log4perl->get_logger( "Git::Demo::Story" );
+    $self->{logger} = $logger;
+
+    if( $self->{verbose} ){
+        $self->{logger}->info( "Running in verbose mode!" );
+    }
+
+
+    $self->{dir} = File::Temp->newdir( UNLINK => 1 );
+
+    if( ! $self->{dir} ){
+        die( "Could not create temporary directory to work in" );
+    }
+    $logger->info( "Working directory: $self->{dir}" );
+    $self->{story} = Git::Demo::Story->new( { story_file => $args->{story_file},
+                                              dir        => $self->{dir},
+                                              verbose    => $self->{verbose},
+                                            } );
+
+    bless $self, $class;
+
+    return $self;
 }
 
-=head2 function2
-
-=cut
-
-sub function2 {
+sub play{
+    my $self = shift;
+    if( ! $self->{story} ){
+        warn( "No story to play!" );
+        return undef;
+    }
+    $self->{story}->play();
 }
+
+
+sub story{
+    my $self = shift;
+    return $self->{story};
+}
+
+sub save_story{
+    my $self = shift;
+    if( $self->{story} ){
+        $self->{story}->save_story();
+    }
+}
+
+sub dir{
+    my $self = shift;
+    return $self->{dir};
+}
+
 
 =head1 AUTHOR
 
